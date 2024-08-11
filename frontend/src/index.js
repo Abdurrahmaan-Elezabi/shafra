82e36d30fb6d15c15c9ef063cba364285bf1f739
Abdurrahmaan-Elezabi <abdoalbaheer4@gmail.com>
2024-08-11 14:11:07 -0700

    Changing the language should now change the text on the webpage to match it

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,9 @@ const answerText = document.getElementById('answer');
 const spinner = document.getElementById('spinner');
 spinner.style.visibility = "hidden";
 const pastAnswers = [];
+const languageDropdown = document.getElementById('language');
+const askTitle = document.getElementById('askTitle');
+const askButton = document.getElementById('askButton');
 
 // When the form's submit button is pressed, do the following:
 // 1. Get the question from the text box
@@ -37,5 +40,16 @@ form.addEventListener('submit', async (event) => {
     console.error(error);
   } finally{
     spinner.style.visibility = "hidden";
+  }
+});
+
+languageDropdown.addEventListener('change', async (event) => {
+  const selectedLanguage = event.target.value;
+  if (selectedLanguage === 'arabic') {
+    askTitle.textContent = 'إسأل سؤالاً';
+    askButton.textContent = 'إسأل';
+  } else {
+    askTitle.textContent = 'Ask a question';
+    askButton.textContent = 'Ask';
   }
 });
